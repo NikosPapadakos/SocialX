2cc56c69ec9a9e307668ed7a798b4fcae26e583d
Nikos Papadakos <74835922+NikosPapadakos@users.noreply.github.com>
2021-03-23 23:25:34 +0200

    d

--- src/components/HomepageElements/Main.js
+++ src/components/HomepageElements/Main.js
@@ -4,7 +4,10 @@ import '../../css/main.css';
 export default function Main() {
     return (
         <div className="feed-wrapper">
-            
+            <div className='feed-friends-toggle'>
+                <span className='feed-btn'>FEED</span>
+                <span className='friends-btn'>FIND FRIENDS</span>
+            </div>
         </div>
     );
 }
