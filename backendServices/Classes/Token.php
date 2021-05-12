e8a4a2f2891f6db670c917acdb564bf1c77a5f7b
Nikos Papadakos <74835922+NikosPapadakos@users.noreply.github.com>
2021-05-12 12:35:30 +0300

    Updates

--- backendServices/Classes/Token.php
+++ backendServices/Classes/Token.php
@@ -14,9 +14,9 @@ class Token
 
 	public function __construct()
 	{
-		$this->key = base64_encode('SocialXNikosPapadakos');
-		$this->iss = 'http://localhost:3000/';
-		$this->aud = 'localhost:3000/';
+		$this->key = base64_encode('mcr_giagos_p');
+		$this->iss = 'http://localhost';
+		$this->aud = 'localhost';
 		$this->iat = time();
 		$this->nbf = time();
 		$this->exp = strtotime('+7 days', time());
@@ -57,12 +57,12 @@ class Token
 			$decoded = JWT::decode($jwt, $this->key, array('HS256'));
 			//Check if token is expired
 			if ($decoded->exp < time())
-				return array("error_code" => 0, "error_message" => "Token expired");
+				return array("error_code" => 1, "error_message" => "Token expired");
 
 			//Valid
 			return array("error_code" => 1, "data" => $decoded->data); // show user details
 		} catch (Exception $e) {
-			return array("error_code" => 0, "error_message" => "Access denied", "error_catch" => $e->getMessage());
+			return array("error_code" => 1, "error_message" => "Access denied", "error_catch" => $e->getMessage());
 		}
 	}
 
--- backendServices/Classes/User.php
+++ backendServices/Classes/User.php
@@ -1,7 +1,5 @@
 <?php
     include_once __DIR__ . '/../Config/Database.php';
-
-
     class User {
         private $conn;
         private $table = 'users';
